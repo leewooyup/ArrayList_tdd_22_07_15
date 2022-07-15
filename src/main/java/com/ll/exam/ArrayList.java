6e17ee79e5a1fec04e20ca517c2d9aae369cb652
leewooyup <zjadlspun1114@naver.com>
2022-07-15 11:13:12 +0900

    기존 함수 추가 구현 및 removeAt() ê, sizeUp() 구현

--- src/main/java/com/ll/exam/ArrayList.java
+++ src/main/java/com/ll/exam/ArrayList.java
@@ -1,14 +1,45 @@
 package com.ll.exam;
 
 public class ArrayList {
+    int[] newArr;
+    int[] arr;
+    int size;
+    public ArrayList() {
+        size = 0;
+        arr = new int[2];
+    }
     public Object size() {
-        return 0;
+        return size;
     }
 
-    public void add(int i) {
+    public void add(int data) {
+        if(size >= arr.length) {
+            sizeUp();
+        }
+        arr[size] = data;
+        size++;
+    }
+
+    private void sizeUp() {
+        newArr = new int[arr.length*2];
+        for(int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
     }
 
     public int get(int i) {
-        return 100;
+        return arr[i];
+    }
+
+    public void removeAt(int index) {
+        for(int i = index; i < size; i++) {
+            arr[i] = arr[i+1];
+        }
+        size--;
+    }
+
+    public int getArrayLength() {
+        return arr.length;
     }
 }
